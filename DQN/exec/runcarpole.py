ff5818b0bf213cdf7a1a3ed2c8e5f8f3b0bde425
martin <martinwangsonghua@gmail.com>
2020-06-30 12:05:09 -0700

    Add files via upload

--- DQN/exec/runcarpole.py
+++ DQN/exec/runcarpole.py
@@ -13,66 +13,80 @@ import sys
 dirName = os.path.dirname(__file__)
 sys.path.append(os.path.join(dirName, '..', '..'))
 sys.path.append(os.path.join(dirName, '..'))
-from src.simpleDQL import dqnmodel
+from src.simpleDQL import UpdateParameters,plotrewards,epsilonDec,BuildModel,ReplayMemory,TrainModel,TrainDQNmodel,Getaction
 from env.Cartpole import CartPoleEnvSetup,visualizeCartpole,resetCartpole,CartPoletransition,CartPoleReward,isTerminal
+from collections import deque
 
 
 
-fixedparameters={
-  'gamma':0.8,
-  'replaysize':20000,
-  'batchsize': 32 ,
-  'epsilon' : 0.4,
-  'learningRate':0.01,
-  'numberlayers':15
-  }
+gamma = 0.9
+buffersize = 20000
+batchsize =  32 
+epsilon = 0.9
+minepsilon = 0.2
+epsilondec = 0.01
+learningRate = 0.01
+numberlayers = 15
+replaceiter = 200
 
 
-EPISODE = 10000 
+EPISODE = 10000
 STEP = 300 
 test = 10
 
 def main():
-  env = CartPoleEnvSetup()        
-  visualize = visualizeCartpole() 
-  reset = resetCartpole()         
-  transition = CartPoletransition()  
-  rewardcart = CartPoleReward()        
-  isterminal = isTerminal()
-  statesdim = env.observation_space.shape[0]
-  actiondim = env.action_space.n
-  DQNmodel = dqnmodel(statesdim,actiondim,fixedparameters)
-  for episode in range(EPISODE):
-    # initialize task
-    state = reset()
-    # Train
-    for step in range(STEP):
-      action = DQNmodel.getaction(state)  
-      next_state=transition(state, action)
-      done = isterminal(next_state)
-      reward = rewardcart(done)
-      DQNmodel.Updatemodel(state, action, reward, next_state,done)
-      state = next_state
-      if done:
-        break
-    if episode % 100 == 0:
-      totalrewards = 0   # total reward
-      for i in range(test):
-        state = reset()
-        for j in range(STEP):
-          visualize(state)
-          action = DQNmodel.getaction(state)      
-          next_state=transition(state, action)
-          done = isterminal(next_state)
-          reward = rewardcart(done)
-          state=next_state
-          totalrewards += reward
-          if done:
-            break
-      meanreward = totalrewards/test
-      print('episode: ',episode,'average Reward:',meanreward)
-      if meanreward >= 200:
-        visualize.close()
-        break
+    env = CartPoleEnvSetup()        
+    visualize = visualizeCartpole() 
+    reset = resetCartpole()         
+    transition = CartPoletransition()  
+    rewardcart = CartPoleReward()        
+    isterminal = isTerminal()
+    
+    statesdim = env.observation_space.shape[0]
+    actiondim = env.action_space.n
+    replaybuffer = deque()
+    runepsilon = epsilon
+    
+    buildmodel = BuildModel(statesdim,actiondim)
+    Writer,DQNmodel = buildmodel(numberlayers)
+    getaction = Getaction(actiondim)
+    updateParameters = UpdateParameters(replaceiter)
+    trainModel = TrainModel(learningRate, gamma,Writer)
+    trainDQNmodel = TrainDQNmodel(updateParameters, trainModel, DQNmodel)
+    replayMemory = ReplayMemory(buffersize,batchsize,trainDQNmodel)
+
+    
+    for episode in range(EPISODE):
+        state  = reset()
+        for step in range(STEP):
+            runepsilon = epsilonDec(runepsilon,minepsilon,epsilondec)
+            action = getaction(DQNmodel,runepsilon,state)  
+            nextstate=transition(state, action)
+            done = isterminal(nextstate)
+            rewards = rewardcart(done)
+            replayMemory(replaybuffer,state, action, rewards, nextstate)
+            state = nextstate
+            if done:
+                break
+        if episode % 100 == 0:
+            totalrewards = 0   # total reward
+            for i in range(test):
+                state = reset()
+                for j in range(STEP):
+                    visualize(state)
+                    action = getaction(DQNmodel,epsilon,state)       
+                    nextstate=transition(state, action)
+                    done = isterminal(nextstate)
+                    reward = rewardcart(done)
+                    state=nextstate
+                    totalrewards += reward
+                    if done:
+                        break
+            meanreward = totalrewards/test
+            print('episode: ',episode,'average Reward:',meanreward)
+            if meanreward >= 200:
+                visualize.close()
+                break
+            plotrewards(rewards, meanreward)
 if __name__ == '__main__':
   main()
