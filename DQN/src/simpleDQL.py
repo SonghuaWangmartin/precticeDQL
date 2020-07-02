82341ab47b37bcb0db06aba989cc9b75a9fdf820
martin <martinwangsonghua@gmail.com>
2020-07-01 19:36:38 -0700

    Add files via upload

--- DQN/src/simpleDQL.py
+++ DQN/src/simpleDQL.py
@@ -11,14 +11,14 @@ import random
 tf.enable_v2_behavior()
 tf.compat.v1.disable_eager_execution()
 
-class UpdateParameters:
+class ReplaceParameters:
     def __init__(self, replaceiter):
         self.replaceiter = replaceiter
     def __call__(self, model,runtime):
         if runtime % self.replaceiter == 0:
             modelgraph = model.graph
-            updateParam_ = modelgraph.get_collection_ref("ReplaceTargetParam_")[0]
-            model.run(updateParam_)
+            replaceParam_ = modelgraph.get_collection_ref("ReplaceTargetParam_")[0]
+            model.run(replaceParam_)
         return model
 
 def samplebuffer(replaybuffer,batchsize):
@@ -26,8 +26,8 @@ def samplebuffer(replaybuffer,batchsize):
     return minibatch
 
 def epsilonDec(epsilon,minepsilon,epsilondec):
-    epsilon = epsilon - epsilondec if epsilon > minepsilon else minepsilon
-    return epsilon
+    runepsilon = epsilon - epsilondec if epsilon > minepsilon else minepsilon
+    return runepsilon
 
 class BuildModel ():
     def __init__(self, statedim, actiondim):
@@ -42,8 +42,8 @@ class BuildModel ():
             with tf.name_scope('inputs'):
                 states_ = tf.placeholder(tf.float32, [None, self.stateDim])
                 nextstates_ = tf.placeholder(tf.float32, [None, self.stateDim])
-                reward_ = tf.placeholder(tf.float32, [None, ])
-                action_ = tf.placeholder(tf.int32, [None, ])
+                reward_ = tf.placeholder(tf.float32, [None])
+                action_ = tf.placeholder(tf.float32, [None, self.actionDim])
                 tf.add_to_collection('states_', states_)
                 tf.add_to_collection('nextstates_', nextstates_)
                 tf.add_to_collection("reward_", reward_)
@@ -91,8 +91,7 @@ class BuildModel ():
                 tf.add_to_collection("Qtarget", Qtarget)
             
             with tf.variable_scope('Qevalaction'):
-                actionind = tf.stack([tf.range(tf.shape(action_)[0], dtype=tf.int32), action_], axis=1)
-                Qevalaction = tf.gather_nd(params=Qevalvalue_, indices=actionind)    # shape=(None, )
+                Qevalaction = tf.reduce_sum(tf.multiply(Qevalvalue_,action_),reduction_indices = 1) 
                 tf.add_to_collection("Qevalaction", Qevalaction)
                 
             with tf.variable_scope('loss'):
@@ -114,12 +113,10 @@ class BuildModel ():
             fullSummary = tf.summary.merge_all()
             tf.add_to_collection("summaryOps", fullSummary)
 
-            Saver = tf.train.Saver(max_to_keep=None)
-            tf.add_to_collection("saver", Saver)
-
-            model = tf.Session(graph=graph)
+            model = tf.Session(graph = graph)
             model.run(tf.global_variables_initializer())
-            Writer = tf.summary.FileWriter('/path/to/logs', graph=graph)
+            
+            Writer = tf.summary.FileWriter('/path/to/logs', graph = graph)
             tf.add_to_collection("Writer", Writer)
         return Writer, model
     
@@ -129,7 +126,6 @@ class TrainModel:
         self.learningRate = learningRate
         self.gamma = gamma
         self.Writer = Writer
-        self.runtime = 0
     def __call__(self, model, minibatch):
         statebatch = [d[0] for d in minibatch]
         actionbatch = [d[1] for d in minibatch]
@@ -143,17 +139,15 @@ class TrainModel:
         learningRate_ = modelGraph.get_collection_ref("learningRate_")[0]
         gamma_ = modelGraph.get_collection_ref("gamma_")[0]
 
-        loss_ = modelGraph.get_collection_ref("loss_")[0]
         trainopt = modelGraph.get_collection_ref("trainopt")[0]
-        Loss, trainOpt = model.run([loss_, trainopt],
-                                        feed_dict={states_: statebatch, action_: actionbatch, reward_: rewardbatch, nextstates_: nextStatebatch,
-                                                   learningRate_: self.learningRate, gamma_: self.gamma})
+        model.run([trainopt],feed_dict={states_: statebatch, action_: actionbatch, reward_: rewardbatch, nextstates_: nextStatebatch,
+                                      learningRate_: self.learningRate, gamma_: self.gamma})
 
         summary = tf.Summary()
         summary.value.add(tag='reward', simple_value=float(np.mean(rewardbatch)))
-        summary.value.add(tag='loss', simple_value=float(Loss))
         self.Writer.flush()
-        return Loss, model
+        
+        return model
     
 class TrainDQNmodel():
     def __init__(self, updateParameters, trainModel, DQNModel):
@@ -162,37 +156,37 @@ class TrainDQNmodel():
         self.DQNModel = DQNModel
         self.runtime = 0
     def __call__(self, miniBatch):
-        Loss, self.DQNModel = self.trainModel(self.DQNModel, miniBatch)
         self.DQNModel = self.updateParameters(self.DQNModel,self.runtime)
+        self.trainModel(self.DQNModel, miniBatch)
         self.runtime += 1
         
-class ReplayMemory():
-    def __init__(self,replaysize,batchsize,trainDQNmodel):
+class Learn():
+    def __init__(self,replaysize,batchsize,trainDQNmodel,actiondim):
         self.replaysize = replaysize
         self.batchsize = batchsize
         self.trainDQNmodel = trainDQNmodel
-    def __call__(self,replaybuffer,state, action, rewards, nextstate):
-        replaybuffer.append((state, action, rewards, nextstate))
+        self.actionDim = actiondim
+    def ReplayMemory(self,replaybuffer,state, action, rewards, nextstate):
+        onehotaction = np.zeros(self.actionDim)
+        onehotaction[action] = 1
+        replaybuffer.append((state, onehotaction, rewards, nextstate))
         if len(replaybuffer) > self.replaysize:
             replaybuffer.popleft()
         if len(replaybuffer) > self.batchsize:
             minibatch = samplebuffer(replaybuffer,self.batchsize)
             self.trainDQNmodel(minibatch)
-        
-class Getaction():
-    def __init__(self, actiondim):
-        self.actionDim = actiondim
-    def __call__(self,model,epsilon,state):
+            
+    def Getaction(self,model,epsilon,state):
         modelGraph = model.graph
         Qevalvalue_ = modelGraph.get_collection_ref("Qevalvalue_")[0]
         states_ = modelGraph.get_collection_ref("states_")[0]
-        state = state[np.newaxis, :]
+        Qvalue = model.run(Qevalvalue_, feed_dict={states_: [state]})
         if np.random.uniform() > epsilon:
-            actions_value = model.run(Qevalvalue_, feed_dict={states_: state})
-            action = np.argmax(actions_value)
+            action = np.argmax(Qvalue)
         else:
-            action = np.random.randint(0, self.actionDim)
+            action = random.choice(np.arange(self.actionDim))
         return action
+      
 
 
 
@@ -206,18 +200,12 @@ def plotrewards(rewards, avgrewards):
     """
     plt.style.use('seaborn-darkgrid')
     fig = plt.figure(figsize=(12,7))
-    ax1 = fig.add_subplot(2, 1, 1)
-    ax2 = fig.add_subplot(2, 1, 2)
+    ax1 = fig.add_subplot(2, 1, 2)
     plt.subplots_adjust(hspace=.5)
     
-    ax1.set_title('Episodic rewards')
-    ax1.plot(rewards, label='Episodic rewards')
+    ax1.set_title('Running rewards')
+    ax1.plot(avgrewards, label='average rewards')
     ax1.set_xlabel("Episodes")
-    ax1.set_ylabel("Rewards")
-    
-    ax2.set_title('Running rewards')
-    ax2.plot(avgrewards, label='average rewards')
-    ax2.set_xlabel("Episodes")
-    ax2.set_ylabel("Average rewards")
+    ax1.set_ylabel("Average rewards")
     
     plt.show(fig)
